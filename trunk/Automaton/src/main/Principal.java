ebe4582ca4bf82fd2ad4f012f26e96e05dd445a3
magdabeamoreno@gmail.com <magdabeamoreno@gmail.com@444a911c-ecb5-11de-9b30-9f9d59cce923>
2010-03-14 08:53:47 +0000

    Se corrigieron errores y se termino de implementar el funcionamiento basico de la aplicacion

--- trunk/Automaton/src/main/Principal.java
+++ trunk/Automaton/src/main/Principal.java
@@ -5,9 +5,11 @@ package main;
 
 import automata.AFD;
 import automata.AFN;
+import io.EntradaSalidaPlano;
 import io.EntradaSalidaXML;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Vector;
 /**
@@ -28,14 +30,33 @@ public class Principal {
             Iterator<AFN> itAFN;
             automatas = ioXML.getAutomatas(xml, xsd);
             itAFN = automatas.iterator();
-            System.out.println(automatas.size());
+            int contador = 0;
+            Vector<AFD> afds = new Vector<AFD>();
+
             while (itAFN.hasNext()) {
                 AFN afn = itAFN.next();
                 AFD afd = afn.toAFD();
+                afds.add(afd);
+
+                System.out.println("AUTOMATA " + ++contador);
+                System.out.println("AFN");
+                System.out.println();
+                System.out.println(afn.toString());
+                System.out.println("AFD");
+                System.out.println();
+                System.out.println(afd.toString());
                 System.out.println("-------------------");
             }
+
+            ioXML.saveAutomatas(new File("afd.xml"), afds);
+
+            EntradaSalidaPlano ioPlano = new EntradaSalidaPlano();
+            ioPlano.saveAutomatas(new File("afd.dat"), afds);
             
         } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        catch (IOException ex) {
             ex.printStackTrace();
         }
 	}
